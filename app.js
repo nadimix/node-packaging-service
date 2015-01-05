c75a2b50342deaab51b24b864b246d7a231e6c26
Nadim El Taha <nadimeltaha@gmail.com>
2015-01-05 11:01:00 +0100

    Adds presets

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response) {
-  response.send('OK');
+app.use(express.static('public'));
+
+var presets = {
+  '480p': '480p at 2 Mbps',
+  '720p': '720p at 3 Mbps',
+  '1080p': '1080p at 5Mbps',
+  '2160p': '2160p at 20Mbps'
+};
+
+app.get('/presets', function(request, response) {
+  response.json(Object.keys(presets));
 });
 
 module.exports = app;
--- test.js
+++ test.js
@@ -2,17 +2,40 @@ var request = require('supertest');
 var app = require('./app');
 
 describe('Requests to the root path', function() {
-  
+
   it('Returns a 200 status code', function(done) {
-    
     request(app)
       .get('/')
-      .expect(200)
-      .end(function(error) {
-        if(error) throw error;
-          done();
-      });
+      .expect(200, done);
+   });
 
+   it('Returns a HTML format', function(done) {
+      request(app)
+        .get('/')
+        .expect('Content-Type', /html/, done);
+   });
+
+});
+
+describe('Requests to the /presets path', function(done) {
+
+   it('Returns a 200 status code', function(done) {
+      request(app)
+        .get('/presets')
+        .expect(200, done);
+   });
+
+   it('Returns a JSON format', function(done) {
+      request(app)
+        .get('/presets')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/, done);
+   });
+
+   it('Returns a set of formats', function(done) {
+      request(app)
+        .get('/presets')
+        .expect(JSON.stringify(["480p","720p","1080p","2160p"]), done);
    });
 
 });
