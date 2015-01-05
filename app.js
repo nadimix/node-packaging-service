5e7c964c9de37cbff50be6c77e2222349c8c8213
Nadim El Taha <nadimeltaha@gmail.com>
2015-01-05 12:42:27 +0100

    Adds new presets

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 var express = require('express');
 var app = express();
+var jsonParser = require('body-parser').json();
 
 app.use(express.static('public'));
 
-var presets = {
-  '480p': '480p at 2 Mbps',
-  '720p': '720p at 3 Mbps',
-  '1080p': '1080p at 5Mbps',
-  '2160p': '2160p at 20Mbps'
-};
+var presets = [
+  { '480p': '480p at 2 Mbps' },
+  { '720p': '720p at 3 Mbps' },
+  { '1080p': '1080p at 5Mbps' },
+  { '2160p': '2160p at 20Mbps' }
+];
 
 app.get('/presets', function(request, response) {
-  response.json(Object.keys(presets));
+  var keys = [];
+  presets.forEach(function (preset) {
+    keys.push(Object.keys(preset));
+  });
+  response.json(JSON.stringify(keys));
+});
+
+app.post('/presets', jsonParser,  function(request, response) {
+  if (!request.body) {
+    return response.status(400).json('request failed');
+  }
+  response.status(201).json(JSON.stringify(presets));
 });
 
 module.exports = app;
--- test.js
+++ test.js
@@ -18,7 +18,7 @@ describe('Requests to the root path', function() {
 });
 
 describe('Requests to the /presets path', function(done) {
-
+    
    it('Returns a 200 status code', function(done) {
       request(app)
         .get('/presets')
@@ -28,14 +28,20 @@ describe('Requests to the /presets path', function(done) {
    it('Returns a JSON format', function(done) {
       request(app)
         .get('/presets')
-        .set('Accept', 'application/json')
         .expect('Content-Type', /json/, done);
    });
 
    it('Returns a set of formats', function(done) {
       request(app)
         .get('/presets')
-        .expect(JSON.stringify(["480p","720p","1080p","2160p"]), done);
+        .expect(JSON.stringify('[["480p"],["720p"],["1080p"],["2160p"]]'), done);
+   });
+
+   it('Creating new formats', function(done) {
+      request(app)
+        .post('/presets')
+        .send(JSON.stringify({'4320p': 'A stunning 8K quality'}))
+        .expect(201, done);
    });
 
 });
