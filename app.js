a5f0f4a68d522332cae836ca7c0bdcd82ae34b72
Nadim El Taha <nadimeltaha@gmail.com>
2015-01-05 17:12:41 +0100

    Place the presets AP using router.route instead of app.APP()

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
 var express = require('express');
 var app = express();
-var jsonParser = require('body-parser').json();
-var client = require('redis').createClient();
-client.select((process.env.NODE_ENV || 'development').length);
 
 app.use(express.static('public'));
 
-app.get('/presets', function(request, response) {
-  client.hkeys('presets', function(error, names) {
-    if(error) throw error;
-    response.json(names);
-  });
-});
-
-app.post('/presets', jsonParser,  function(request, response) {
-  var newPreset = request.body;
-  if (!newPreset.name || !newPreset.description) {
-    response.status(400);
-    return false;
-  }
-  client.hset('presets', newPreset.name, newPreset.description, function(error) {
-    if(error) throw error;
-    response.status(201).json(newPreset.name);
-  });
-});
+var presets = require('./routes/presets');
+app.use('/presets', presets);
 
 module.exports = app;
---
+++ routes/presets.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+var jsonParser = require('body-parser').json();
+var client = require('redis').createClient();
+client.select((process.env.NODE_ENV || 'development').length);
+
+var router = express.Router();
+
+router.route('/')
+  .get(function(request, response) {
+    client.hkeys('presets', function(error, names) {
+      if(error) throw error;
+      response.json(names);
+    });
+  })
+  
+  .post(jsonParser,  function(request, response) {
+    var newPreset = request.body;
+    if (!newPreset.name || !newPreset.description) {
+      response.status(400);
+      return false;
+    }
+    client.hset('presets', newPreset.name, newPreset.description, function(error) {
+      if(error) throw error;
+      response.status(201).json(newPreset.name);
+    });
+  });
+
+module.exports = router;
